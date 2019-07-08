781ab4aab4f0634c48335d8477e75d27089ada78
Sam Barrie <sam.dbarrie@gmail.com>
2019-07-08 14:03:07 +0100

    removed password_digest from users

--- beer-or-horse-backend/db/schema.rb
+++ beer-or-horse-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_08_110737) do
+ActiveRecord::Schema.define(version: 2019_07_08_130143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 2019_07_08_110737) do
 
   create_table "users", id: :serial, force: :cascade do |t|
     t.string "email"
-    t.string "password_digest"
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
