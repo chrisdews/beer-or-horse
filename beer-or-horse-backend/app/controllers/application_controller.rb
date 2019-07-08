a950a53c1e1f9d9b4d02a1702c8d09fc95cc5769
Alex Johnston <alexjohnston@ymail.com>
2019-07-08 18:57:39 +0100

    started javascript and backend controllers

--- beer-or-horse-backend/app/controllers/application_controller.rb
+++ beer-or-horse-backend/app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::API
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
   include SessionsHelper
 
   before_action :current_user
-  helper_method :logged_in?
-  helper_method :authorize_user
-  helper_method :current_user
+  # helper_method :logged_in?
+  # helper_method :authorize_user
+  # helper_method :current_user
 
   def hello
     redirect_to controller: 'sessions', action: 'new' unless session[:name]
