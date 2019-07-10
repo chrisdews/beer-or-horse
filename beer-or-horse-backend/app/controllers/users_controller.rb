91f3e2e2bdcdbe0546104af27d18d71076fce07d
Sam Barrie <sam.dbarrie@gmail.com>
2019-07-10 11:27:25 +0100

    added top score to users

--- beer-or-horse-backend/app/controllers/users_controller.rb
+++ beer-or-horse-backend/app/controllers/users_controller.rb
@@ -37,6 +37,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:name, :top_score)
   end
 end
--- beer-or-horse-backend/db/schema.rb
+++ beer-or-horse-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_08_173646) do
+ActiveRecord::Schema.define(version: 2019_07_10_102614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2019_07_08_173646) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "top_score"
   end
 
   add_foreign_key "beer_questions", "beers"
